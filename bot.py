633b5973e6ce97e25510c6fac2cae754e92c4e74
terry <qh384786@gmail.com>
2024-02-10 18:55:29 -0400

    fixed the a bug with the emoji icons not working

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord import emoji
 from discord.ext import commands
 import os
 import asyncio
@@ -9,7 +10,7 @@ client = commands.Bot(command_prefix='=', intents=discord.Intents.all())
 @client.event
 async def on_ready():
     await client.tree.sync()
-    print(f'{client.user} is connected to the server.')
+    print(f"{client.user} is connected to the server.")
 
 
 class SelfPosition(discord.ui.View):
@@ -119,7 +120,7 @@ class SelfRanks(discord.ui.View):
 @client.tree.command(name="ping", description="show latency of the bot")
 async def ping(interaction: discord.Interaction):
     bot_latency = round(client.latency * 1000)
-    await interaction.response.send_message(f"<:Position_DiamondTop:1202765203557584906>")
+    await interaction.response.send_message(f"<:DiamondTop:1204243685445865543>")
 
 
 @client.tree.command(name="selfroles", description="give yourself a role")
@@ -129,47 +130,48 @@ async def self_role(interaction: discord.Interaction):
 
 @client.tree.command(name="createrole", description="create all the roles")
 async def createrole(interaction: discord.Interaction):
-    all_positions = ["Top", "Jng", "Mid", "Bot", "Sup"]
-    all_ranks = ["Iron", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Grandmaster", "Challenger"]
-    all_icons = ['<:IronTop:1204243791750627328>', ' <:BronzeTop:1204243626553774151>', ' <:Position_SilverTop:1202765001644056606>',
-                 '<:GoldTop:1204243717653794857>',' <:Position_PlatTop:1202764964465483847>',' <:DiamondTop:1204243685445865543>',' <:Position_MasterTop:1202764932895215626>',
-                 '<:GrandmasterTop:1204243753292931122>',' <:ChallengerTop:1204243653900501063>',
+    all_positions = ["Top", "Jungle", "Mid", "Bot", "Support"]
+    all_ranks = ["Iron", "Bronze", "Silver", "Gold", "Plat", "Diamond", "Master", "Grandmaster", "Challenger"]
+    all_icons = ["<:IronTop:1204243791750627328>", "<:BronzeTop:1204243626553774151>", "<:SilverTop:1205964421629874206>",
+                 "<:GoldTop:1204243717653794857>","<:PlatTop:1205960294107582594>","<:DiamondTop:1204243685445865543>","<:MasterTop:1204475730352672869>",
+                 "<:GrandmasterTop:1204243753292931122>"," <:ChallengerTop:1204243653900501063>",
 
-                 '<:IronJungle:1204243787245813882>', '<:BronzeJungle:1204243620287488112>',' <:Position_SilverJungle:1202764996308762624>',
-                 '<:GoldJungle:1204243713480589312>', ' <:Position_PlatJungle:1202764961080672298>' ,'<:DiamondJungle:1204243680790183966>', '<:Position_MasterJungle:1202764928272826458>',
-                 ' <:GrandmasterJungle:1204243748784177232>', '<:ChallengerJungle:1204243648250777601>',
+                 "<:IronJungle:1204243787245813882>", "<:BronzeJungle:1204243620287488112>","<:SilverJungle:1205964417913589820>",
+                 "<:GoldJungle:1204243713480589312>", "<:PlatJungle:1205960288084557875>" ,"<:DiamondJungle:1204243680790183966>", "<:MasterJungle:1204475720772882473>",
+                 "<:GrandmasterJungle:1204243748784177232>", "<:ChallengerJungle:1204243648250777601>",
 
-                 '<:IronMid:1204243788185468929>', '<:BronzeMid:1204243622732759111>', ' <:Position_SilverMid:1202764998162784306>',
-                 ' <:GoldMid:1204243714638356600>','<:Position_PlatMid:1202764962003558471>', ' <:DiamondMid:1204243682698723348>', '<:Position_MasterMid:1202764930013728779>',
-                 ' <:GrandmasterMid:1204243749836820571>', ' <:ChallengerMid:1204243650582806598>',
+                 "<:IronMid:1204243788185468929>", "<:BronzeMid:1204243622732759111>", "<:SilverMid:1205964419251568712>",
+                 "<:GoldMid:1204243714638356600>","<:PlatMid:1205960291318505562>", " <:DiamondMid:1204243682698723348>", "<:MasterMid:1204475724917116958>",
+                 "<:GrandmasterMid:1204243749836820571>", " <:ChallengerMid:1204243650582806598>",
 
-                 '<:IronBot:1204243785534414888>', '<:BronzeBot:1204243618580267088>', ' <:Position_SilverBot:1202764995306332160>',
-                 '<:GoldBot:1204243711186182234>', ' <:Position_PlatBot:1202764959499554906>' ,'<:DiamondBot:1204243679502409798>', ' <:Position_MasterBot:1202764927081775134>',
-                 '<:GrandmasterBot:1204243747215384576>', '<:ChallengerBot:1204243646937825290>',
+                 "<:IronBot:1204243785534414888>", "<:BronzeBot:1204243618580267088>", "<:SilverBot:1205964416336662578>",
+                 "<:GoldBot:1204243711186182234>", "<:PlatBot:1205960287019208734>" ,"<:DiamondBot:1204243679502409798>", "<:MasterBot:1204475689550749767>",
+                 "<:GrandmasterBot:1204243747215384576>", "<:ChallengerBot:1204243646937825290>",
 
-                 '<:IronSupport:1204243789967921163>', ' <:BronzeSupport:1204243625169649685>', ' <:Position_SilverSupport:1202764999446106223>',
-                 ' <:GoldSupport:1204243715787460668>', ' <:Position_PlatSupport:1202764962901004299>' ,' <:DiamondSupport:1204243683705225246>', ' <:Position_MasterSupport:1202764931443986433>,'
-                 ' <:GrandmasterSupport:1204243751015551046>', ' <:ChallengerSupport:1204243651811876885>'
+                 "<:IronSupport:1204243789967921163>", " <:BronzeSupport:1204243625169649685>", "<:SilverSupport:1205964420594012220>",
+                  "<:GoldSupport:1204243715787460668>", "<:PlatSupport:1205960292731981845>" ," <:DiamondSupport:1204243683705225246>", "<:MasterSupport:1204475719661395978>",
+                  "<:GrandmasterSupport:1204243751015551046>", "<:ChallengerSupport:1204243651811876885>"
                  ]
-    count = 0
     for all_position in all_positions:
         for all_rank in all_ranks:
-            count += 1
             posrank = all_rank + all_position
-            await interaction.guild.create_role(name=posrank, display_icon=all_icons[count - 1])
-            await interaction.response.send_message(content="role created")
-
+            src = "ranked-positions/" + posrank + ".png"
+            with open(src, 'rb') as emoji_file:
+                emoji_data = emoji_file.read()
+            await interaction.guild.create_role(name=posrank, display_icon=emoji_data)
+            emoji_file.close()
+            await interaction.response.send_message(content="role created: " + src)
 
 async def load():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
-            await client.load_extension(f'cogs.{filename[:-3]}')
+            await client.load_extension(f"cogs.{filename[:-3]}")
 
 
 async def main():
     async with client:
         await load()
-        await client.start("YOUR_TOKEN")
+        await client.start("MTIwMjY3NTQ3MTExNzE5NzQwMg.GHaamE.2Y4lX7k2_e1gIdLsnhGHoJ37jH9lJPJbNlDXpw")
 
 
 asyncio.run(main())
